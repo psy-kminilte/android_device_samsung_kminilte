caca0dfc262f1e3e2e2df1d4f967b89f257b69b1
psyafter <psyafter@gmail.com>
2016-01-23 19:04:18 +0200

    initial
    Change-Id: I4811bcb93c7f05eeda559455cbca6e493c7f2f3a

--- tesla.mk
+++ tesla.mk
@@ -1,14 +1,14 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/kminilte/full_kminilte.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kminilte
-PRODUCT_NAME := validus_kminilte
+PRODUCT_NAME := tesla_kminilte
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := SM-G800
 
