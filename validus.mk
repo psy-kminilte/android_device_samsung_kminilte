ea99766a5005797df256a10404d9f869d2366977
psyafter <psyafter@gmail.com>
2016-12-16 11:28:11 +0200

    validus

--- validus.mk
+++ validus.mk
@@ -1,12 +1,12 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
-$(call inherit-product, vendor/tesla/config/caf_fw.mk)
+# Inherit some common Validus stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
 
 $(call inherit-product, device/samsung/kminilte/full_kminilte.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kminilte
-PRODUCT_NAME := tesla_kminilte
+PRODUCT_NAME := validus_kminilte
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := SM-G800
 
