991b91b099d4c4ad56b04bf6ead40deae854c8c1
psyafter <psyafter@gmail.com>
2017-02-03 10:51:16 +0200

    Revert "Set wifi calling props"
    This reverts commit c10988c6bfe76eec214bab2f49fd56e8ba0afa95.

--- device.mk
+++ device.mk
@@ -155,16 +155,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     wifiloader
 
-# WiFi calling
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.iwlan.enable=true \
-    persist.radio.ignore_ims_wlan=1 \
-    persist.radio.data_con_rprt=1
-
 # Store correct IMSI when retreived from SIMRecords and use it for RuimRecords
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.get_imsi_from_sim=true
-
 
 # Dalvik VM specific for devices with 2048 MB of RAM (G800F has 1.5G, but 2G config seems to fit)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
